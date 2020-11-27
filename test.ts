56696ac795a0b058f6a6f22c95ba076cee7ccb62
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-11-27 02:46:25 -0800

    more of the protocol

--- test.ts
+++ test.ts
@@ -1,11 +1,19 @@
-console.log(`connecting to https://www.wss-websocket.net/`)
-const ws = new WebSocket("https://www.wss-websocket.net/")
-ws.onmessage = (msg) => {
-    const data = msg.data;
-    console.log(`message ${typeof data}`)
-    console.log(data)
-}
-ws.onopen = () => {
-    ws.send(JSON.stringify({ message: "hello from makecode"}));
-    ws.send(hex`0a0b0c0d0e0f`);
-}
+control.runInParallel(function() {
+    console.log(`connecting to https://www.wss-websocket.net/`)
+    const ws = new WebSocket("https://www.wss-websocket.net/")
+    console.log(`socket state: ${ws.readyState}`)
+    ws.onmessage = (msg) => {
+        const data = msg.data;
+        console.log(`message ${typeof data}`)
+        console.log(data)
+    }
+    ws.onopen = () => {
+        while(true) {
+            console.log(`socket state: ${ws.readyState}`)
+            ws.send(JSON.stringify({ message: "hello from makecode", time: control.millis() }));    
+            pause(1000)
+            ws.send(Buffer.fromArray([control.millis()]));
+            pause(1000)
+        }
+    }    
+})
