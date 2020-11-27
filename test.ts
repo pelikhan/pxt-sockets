01a22e67fbea69d0c54e0c4ed8729ed32490293d
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-11-27 05:28:47 -0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,22 +1,44 @@
 game.consoleOverlay.setVisible(true)
 control.runInParallel(function() {
     console.log(`connecting to https://www.websocket.org/echo.html`)
-    const ws = new WebSocket("wss://echo.websocket.org")
-    ws.onerror = () => console.log("error")
-    ws.onmessage = (msg) => {
-        const data = msg.data;
-        console.log(`--> ${typeof data === "string" ? data : data.toHex()}`)
+
+    {
+        // string tests
+        const ws = new WebSocket("wss://echo.websocket.org")
+        ws.onerror = () => console.log("error")
+        ws.onmessage = (msg) => {
+            const data = msg.data;
+            console.log(`--> ${data}`)
+        }
+        ws.onopen = () => {
+            forever(() => {
+                const m = `makecode ${control.millis()}`;
+                console.log(`<-- ${m}`)
+                ws.send(m);    
+                pause(5000)
+                const b = Buffer.fromArray([control.millis()]);
+                console.log(`<-- ${b.toHex()}`)
+                ws.send(b);
+                pause(5000)
+            })
+        }    
     }
-    ws.onopen = () => {
-        forever(() => {
-            const m = `makecode ${control.millis()}`;
-            console.log(`<-- ${m}`)
-            ws.send(m);    
-            pause(5000)
-            const b = Buffer.fromArray([control.millis()]);
-            console.log(`<-- ${b.toHex()}`)
-            ws.send(b);
-            pause(5000)
-        })
-    }    
+
+    {
+        // binary tests
+        const ws = new WebSocket("wss://echo.websocket.org")
+        ws.onerror = () => console.log("error")
+        ws.onmessage = (msg) => {
+            const data = msg.data;
+            console.log(`--> ${(data as Buffer).toHex()}`)
+        }
+        ws.onopen = () => {
+            forever(() => {
+                const b = Buffer.fromArray([control.millis()]);
+                console.log(`<-- ${b.toHex()}`)
+                ws.send(b);
+                pause(5000)
+            })
+        } 
+    }
 })
