974e49400df4ee6a002ef72ba8b0321f7479999a
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-11-27 02:07:31 -0800

    udpated example

--- test.ts
+++ test.ts
@@ -1,5 +1,11 @@
 const ws = new WebSocket("https://www.wss-websocket.net/")
 
+ws.onmessage = (msg) => {
+    const data = msg.data;
+    console.log(`message ${typeof data}`)
+    console.log(data)
+}
 ws.onopen = () => {
     ws.send(JSON.stringify({ message: "hello from makecode"}));
+    ws.send(hex`0a0b0c0d0e0f`);
 }
